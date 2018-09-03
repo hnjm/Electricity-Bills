320bba928360f095dfff7300775e6dce143f18f1
dabas <dabas4programming@gmail.com>
2018-09-03 15:31:18 +0300

    Install MahApps && MaterialDesign

--- ElectricityBills/MainWindow.xaml.cs
+++ ElectricityBills/MainWindow.xaml.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Linq;
 using System.Windows;
+using MahApps.Metro.Controls;
 
 namespace ElectricityBills
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow 
     {
         public MainWindow()
         {
